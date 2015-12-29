a4d5e07c89ea6887fb8a8a9db0357525310a14a8
Thamme Gowda N. <tgowdan@gmail.com>
2015-12-29 00:31:06 -0800

    Add a testcase and sample test html files

---
+++ src/test/java/edu/usc/cs/autoext/tree/ZSTEDComputerTest.java
@@ -0,0 +1,33 @@
+package edu.usc.cs.autoext.tree;
+
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tg on 12/29/15.
+ */
+public class ZSTEDComputerTest {
+
+    @Test
+    public void testMain() throws Exception {
+        ClassLoader resLoader = getClass().getClassLoader();
+        String file1 = resLoader.getResource("html/simple/1.html").getPath();
+        String file2 = resLoader.getResource("html/simple/2.html").getPath();
+        String file3 = resLoader.getResource("html/simple/3.html").getPath();
+        //same file
+        double distance = ZSTEDComputer.computeDistance(new File(file1), new File(file1));
+        assertEquals(0.0, distance, 0.00);
+
+        //almost same
+        distance = ZSTEDComputer.computeDistance(new File(file1), new File(file2));
+        assertEquals(3.0, distance, 0.00);
+
+        //dissimilar
+        distance = ZSTEDComputer.computeDistance(new File(file1), new File(file3));
+        System.out.println(distance);
+
+    }
+}
