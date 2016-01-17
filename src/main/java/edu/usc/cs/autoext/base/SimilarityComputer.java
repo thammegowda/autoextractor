d964038518adbdc55a7974e9a2d9b2c11b0ee4f6
Thamme Gowda N. <tgowdan@gmail.com>
2016-01-16 18:23:57 -0800

    Similarity computer interface/contract added

---
+++ src/main/java/edu/usc/cs/autoext/base/SimilarityComputer.java
@@ -0,0 +1,24 @@
+package edu.usc.cs.autoext.base;
+
+import edu.usc.cs.autoext.tree.StructureSimilarityComputer;
+import edu.usc.cs.autoext.tree.StyleSimilarityComputer;
+
+/**
+ * Generic Similarity computer contract. Look into the implementations for specific details
+ * @see StructureSimilarityComputer
+ * @see  StyleSimilarityComputer
+ * @author Thamme Gowda
+ *
+ */
+public interface SimilarityComputer<T> {
+
+    /**
+     * computes similarity between two objects. The similarity score is on [0.0, 1.0] scale inclusive.
+     * The score of 1.0 indicates that argument {@code obj1} and {@code obj2} are extremely similar.
+     * Similarity score of 0.0 indicates that both input objects are extremely dissimilar.
+     * @param obj1 the first object
+     * @param obj2  the second object
+     * @return the similarity score [0.0, 1.0]
+     */
+    double compute(T obj1, T obj2);
+}
